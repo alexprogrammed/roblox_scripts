654e17800d1f35d9868479f50ed335bbaaabcaea
nilkibite <96582027+nilkibite@users.noreply.github.com>
2022-09-21 05:25:09 -0600

    Added climb mechanic.
    This is kind of scuffed but it works.

--- scripts/celeste.lua
+++ scripts/celeste.lua
@@ -14,6 +14,7 @@ local root:BasePart = character:WaitForChild("HumanoidRootPart", math.huge)
 local camera:Camera = Workspace.CurrentCamera
 
 local canDash:boolean = true
+local isClimbing:boolean = false
 
 local connections:{RBXScriptConnection} = {}
 
@@ -29,7 +30,7 @@ table.insert(connections, UserInputService.InputBegan:Connect(function(input:Inp
 			for i = 1, fps / 5 do
 				task.delay(lastDelay * i, function()
 					for _, part in pairs(character:GetChildren()) do
-						if part:IsA("BasePart") then
+						if part:IsA("BasePart") then					
 							part.AssemblyLinearVelocity = lookVector * 75
 						end
 					end
@@ -37,13 +38,45 @@ table.insert(connections, UserInputService.InputBegan:Connect(function(input:Inp
 			end
 		end
 	end
+	if input.KeyCode == Enum.KeyCode.Z then
+		isClimbing = true
+	end
+end))
+
+table.insert(connections, UserInputService.InputEnded:Connect(function(input:InputObject, gameProcessed:boolean)
+	if gameProcessed then return end
+	if input.KeyCode == Enum.KeyCode.Z then
+		isClimbing = false
+	end
 end))
 
 table.insert(connections, RunService.RenderStepped:Connect(function(d)
 	lastDelay = d
 	local state = humanoid:GetState()
+	
 	if state == Enum.HumanoidStateType.Running then
 		canDash = true
+	end
+	
+	if isClimbing then
+		local ray = game:GetService("Workspace"):Raycast(root.Position - Vector3.new(0, 1, 0), (root.CFrame * CFrame.new(0, -1, 0)).LookVector * 2)
+		if ray then
+			local vel = 0
+			
+			if UserInputService:IsKeyDown(Enum.KeyCode.E) then
+				vel = humanoid.WalkSpeed
+			elseif UserInputService:IsKeyDown(Enum.KeyCode.Q) then
+				vel = -humanoid.WalkSpeed
+			end
+			
+			for _, part in pairs(character:GetChildren()) do
+				if part:IsA("BasePart") then					
+					part.AssemblyLinearVelocity = Vector3.new(part.AssemblyLinearVelocity.X, vel, part.AssemblyLinearVelocity.Z)
+				end
+			end
+		else
+			isClimbing = false
+		end
 	end
 end))
 
