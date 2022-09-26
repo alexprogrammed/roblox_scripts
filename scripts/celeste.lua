955e16ac973891c2e6532fa615cb56d56e368ba0
nilkibite <96582027+nilkibite@users.noreply.github.com>
2022-09-25 19:12:59 -0600

    Update celeste.lua

--- scripts/celeste.lua
+++ scripts/celeste.lua
@@ -5,23 +5,23 @@ local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 local Workspace = game:GetService("Workspace")
 
-local player:Player? = Players.LocalPlayer
-local character:Model = player.Character or player.CharacterAdded:Wait()
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
 
-local humanoid:Humanoid = character:WaitForChild("Humanoid", math.huge)
-local root:BasePart = character:WaitForChild("HumanoidRootPart", math.huge)
+local humanoid = character:WaitForChild("Humanoid", math.huge)
+local root = character:WaitForChild("HumanoidRootPart", math.huge)
 
-local camera:Camera = Workspace.CurrentCamera
+local camera = Workspace.CurrentCamera
 
-local canDash:boolean = true
-local isClimbing:boolean = false
+local canDash = true
+local isClimbing = false
 
-local connections:{RBXScriptConnection} = {}
+local connections = {}
 
-local lastDelay:number = 0
-local lastJump:number = os.clock()
+local lastDelay = 0
+local lastJump = os.clock()
 
-table.insert(connections, UserInputService.InputBegan:Connect(function(input:InputObject, gameProcessed:boolean)
+table.insert(connections, UserInputService.InputBegan:Connect(function(input, gameProcessed)
 	if gameProcessed then return end
 	
 	if input.KeyCode == Enum.KeyCode.X then
@@ -44,17 +44,17 @@ table.insert(connections, UserInputService.InputBegan:Connect(function(input:Inp
 	if input.KeyCode == Enum.KeyCode.Space then
 		if humanoid:GetState() == Enum.HumanoidStateType.Climbing or humanoid:GetState() == Enum.HumanoidStateType.Running then return end
 		
-		local params:OverlapParams = RaycastParams.new()
+		local params = RaycastParams.new()
 		params.FilterType = Enum.RaycastFilterType.Blacklist
 		params.FilterDescendantsInstances = character:GetChildren()
 		
-		local ray:RaycastResult = game:GetService("Workspace"):Raycast(root.Position, root.CFrame.LookVector * 2, params)
+		local ray = game:GetService("Workspace"):Raycast(root.Position, root.CFrame.LookVector * 2, params)
 		
 		if ray then
-			local instance:Instance = ray.Instance
-			local normal:Vector3 = ray.Normal
+			local instance = ray.Instance
+			local normal = ray.Normal
 			
-			local pivotTo:CFrame = CFrame.lookAt(root.Position, root.Position - normal)
+			local pivotTo = CFrame.lookAt(root.Position, root.Position - normal)
 			character:PivotTo(pivotTo)
 			
 			for _, part in pairs(character:GetChildren()) do
@@ -74,7 +74,7 @@ end))
 
 table.insert(connections, RunService.RenderStepped:Connect(function(d)
 	lastDelay = d
-	local state:Enum.HumanoidStateType = humanoid:GetState()
+	local state = humanoid:GetState()
 	
 	if state == Enum.HumanoidStateType.Running then
 		canDash = true
@@ -83,21 +83,21 @@ table.insert(connections, RunService.RenderStepped:Connect(function(d)
 	if isClimbing then
 		if os.clock() - lastJump < 0.1 then return end
 		
-		local params:OverlapParams = RaycastParams.new()
+		local params = RaycastParams.new()
 		params.FilterType = Enum.RaycastFilterType.Blacklist
 		params.FilterDescendantsInstances = character:GetChildren()
 		
-		local ray:RaycastResult = game:GetService("Workspace"):Raycast(root.Position - Vector3.new(0, 1, 0), (root.CFrame * CFrame.new(0, -1, 0)).LookVector * 2, params)
+		local ray = game:GetService("Workspace"):Raycast(root.Position - Vector3.new(0, 1, 0), (root.CFrame * CFrame.new(0, -1, 0)).LookVector * 2, params)
 		
 		if ray then
-			local instance:Instance = ray.Instance
-			local normal:Vector3 = ray.Normal
-			local distance:number = ray.Distance
+			local instance = ray.Instance
+			local normal = ray.Normal
+			local distance = ray.Distance
 			
-			local size:Vector3 = instance.Size
-			local cframe:CFrame = instance.CFrame
+			local size = instance.Size
+			local cframe = instance.CFrame
 			
-			local pivotTo:CFrame = CFrame.lookAt(root.Position, root.Position - (normal))
+			local pivotTo = CFrame.lookAt(root.Position, root.Position - (normal))
 			
 			pivotTo *= CFrame.new(0, 0, - (distance - (humanoid.RigType == Enum.HumanoidRigType.R6 and 0.6 or 1.1)))
 			character:PivotTo(pivotTo)
@@ -114,7 +114,7 @@ table.insert(connections, RunService.RenderStepped:Connect(function(d)
 end))
 
 humanoid.Died:Connect(function()
-	for _, connection:RBXScriptConnection in pairs(connections) do
+	for _, connection in pairs(connections) do
 		connection:Disconnect()
 	end
 end)
