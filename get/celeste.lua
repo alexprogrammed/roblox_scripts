cd46ff7f9cf75a57ed7a8f4aa02444b14e9d85f3
nilkibite <96582027+nilkibite@users.noreply.github.com>
2022-09-28 23:10:59 -0600

    Delete get directory

--- get/celeste.lua
+++
@@ -1,6 +0,0 @@
-getgenv().config = {
-  dash = Enum.KeyCode.X,
-  climb = Enum.KeyCode.Z
-}
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nilkibite/roblox_scripts/main/scripts/celeste.lua"))()
