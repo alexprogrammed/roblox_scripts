0dac75b8758bbbf6a3710c921dfe985b16ed33d6
nilkibite <96582027+nilkibite@users.noreply.github.com>
2022-09-29 11:00:55 -0600

    Delete init directory

--- init/checkpoints.lua
+++
@@ -1,7 +0,0 @@
-getgenv().config = {
-  set = Enum.KeyCode.E,
-  teleport = Enum.KeyCode.R,
-  unset = Enum.KeyCode.Q
-}
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nilkibite/roblox_scripts/main/scripts/checkpoints.lua"))()
