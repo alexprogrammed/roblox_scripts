1a686d21a317a4507862a28fce994d06fc80fc8c
nilkibite <96582027+nilkibite@users.noreply.github.com>
2022-09-28 22:47:35 -0600

    Create celeste.lua

---
+++ get/celeste.lua
@@ -0,0 +1,6 @@
+getgenv().config = {
+  dash = Enum.KeyCode.X,
+  climb = Enum.KeyCode.Z
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nilkibite/roblox_scripts/main/scripts/celeste.lua"))()
