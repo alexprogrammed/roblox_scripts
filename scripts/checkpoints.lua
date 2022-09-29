fdd791c7ede83a8db846d8476ed16712d559dc5b
nilkibite <96582027+nilkibite@users.noreply.github.com>
2022-09-29 02:02:10 -0600

    new gui

--- scripts/checkpoints.lua
+++ scripts/checkpoints.lua
@@ -5,15 +5,109 @@ if not config then config = {set = Enum.KeyCode.E, teleport = Enum.KeyCode.R, un
 local Workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
 local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local TweenService = game:GetService("TweenService")
 
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
-local clock = os.clock()
+
+function randomString()
+	local str = ""
+	for i = 1, 100 do str = str .. string.char(math.random(1,127)) end
+	return str
+end
 
 local checkpoints = {}
 local checkpointf = {}
 
-function checkpointf.unset()
+function checkpointf:gui()
+	local gui, detector, text, frame = Instance.new("ScreenGui"), Instance.new("Frame"), Instance.new("TextLabel"), Instance.new("Frame")
+	gui.DisplayOrder, gui.Name, gui.IgnoreGuiInset = 2147483647, randomString(), true
+	detector.BackgroundTransparency, detector.Position, detector.Size, detector.Name, detector.Parent = 1, UDim2.new(0, 0, 0.8, 0), UDim2.new(1, 0, 0.2, 0), randomString(), gui
+	frame.Size, frame.Position, frame.Name, frame.BackgroundTransparency, frame.Parent = UDim2.new(1, 0, 0.2, 0), UDim2.new(0, 0, 1, 0), randomString(), 1, gui
+	text.Size, text.Position, text.Name, text.BackgroundTransparency, text.Text, text.TextScaled, text.Font, text.TextColor3, text.TextStrokeTransparency, text.TextStrokeColor3, text.Parent = UDim2.new(1, 0, 0.5, 0), UDim2.new(0, 0, 0.5, 0), randomString(), 1, "", true, Enum.Font.Ubuntu, Color3.new(1, 1, 1), 0, Color3.new(0, 0, 0), frame
+	
+	local connections = {
+		
+	}
+	
+	local messages = {
+		config.set.Name..": set",
+		config.unset.Name..": unset",
+		config.teleport.Name..": teleport"
+	}
+	
+	detector.MouseEnter:Connect(function()
+		for _, connection in pairs(connections) do
+			connection:Disconnect()
+		end
+
+		local lerpStart = os.clock()
+		local connection
+		connection = RunService.RenderStepped:Connect(function()
+			if os.clock() - lerpStart > 0.5 then
+				connection:Disconnect()
+				frame.Position = UDim2.new(0, 0, 0.9, 0)
+			else
+				frame.Position = UDim2.new(0, 0, 1, 0):Lerp(UDim2.new(0, 0, 0.9, 0), os.clock() - lerpStart)
+			end
+		end)
+		table.insert(connections, connection)
+	end)
+	
+	detector.MouseLeave:Connect(function()
+		for _, connection in pairs(connections) do
+			connection:Disconnect()
+		end
+		
+		local lerpStart = os.clock()
+		local connection
+		connection = RunService.RenderStepped:Connect(function()
+			if os.clock() - lerpStart > 0.5 then
+				connection:Disconnect()
+				frame.Position = UDim2.new(0, 0, 1, 0)
+			else
+				frame.Position = UDim2.new(0, 0, 0.9, 0):Lerp(UDim2.new(0, 0, 1, 0), os.clock() - lerpStart)
+			end
+		end)
+		table.insert(connections, connection)
+	end)
+	
+	task.spawn(function()
+		while true do
+			for i = 1, #messages do
+				local message = messages[i]
+				text.Text = message
+				
+				local tweenStartA = os.clock()
+				while os.clock() - tweenStartA < 0.5 do
+					text.Position = UDim2.new(0, 0, 0.5, 0):Lerp(UDim2.new(0, 0, -0.1, 0), (os.clock() - tweenStartA) * 2)
+					task.wait()
+				end
+				
+				text.Position = UDim2.new(0, 0, -0.1, 0)
+				task.wait(0.5)
+				
+				local tweenStartB = os.clock()
+				while os.clock() - tweenStartB < 0.5 do
+					text.Position = UDim2.new(0, 0, -0.1, 0):Lerp(UDim2.new(0, 0, 0.5, 0), (os.clock() - tweenStartB) * 2)
+					task.wait()
+				end
+				
+				text.Position = UDim2.new(0, 0, 0.5, 0)
+			end
+		end
+	end)
+	
+	if syn then
+		syn.protect_gui(gui)
+		gui.Parent = game:GetService("CoreGui")
+	else
+		gui.Parent = player:FindFirstChildOfClass("PlayerGui")
+	end
+end
+
+function checkpointf:unset()
 	if #checkpoints > 0 then
 		local checkpoint = checkpoints[#checkpoints]
 		
@@ -25,7 +119,7 @@ function checkpointf.unset()
 	end
 end
 
-function checkpointf.set()
+function checkpointf:set()
 	if character then
 		local root = character.PrimaryPart
 		if root then
@@ -37,7 +131,7 @@ function checkpointf.set()
 	end
 end
 
-function checkpointf.teleport()
+function checkpointf:teleport()
 	if #checkpoints > 0 then
 		if character then
 			local root = character.PrimaryPart
@@ -95,15 +189,16 @@ function _hook()
 	end))
 end
 
-function characterAdded()
-	character = player.Character
+function characterAdded() character = player.Character end
+
+function inputBegan(input, gameProcessed) if gameProcessed then return end
+	if input.KeyCode == config.set then checkpointf:set() end
+	if input.KeyCode == config.unset then checkpointf:unset() end
+	if input.KeyCode == config.teleport then checkpointf:teleport() end
 end
 
-function inputBegan(input, gameProcessed)
-	if gameProcessed then return end
-	if input.KeyCode == config.set then checkpointf.set() end
-	if input.KeyCode == config.unset then checkpointf.unset() end
-	if input.KeyCode == config.teleport then checkpointf.teleport() end
+function mouseEnter()
+	
 end
 
 UserInputService.InputBegan:Connect(inputBegan)
@@ -112,3 +207,5 @@ player.CharacterAdded:Connect(characterAdded)
 if syn then
 	_hook()
 end
+
+checkpointf:gui()
