714bf7f6f2d850d656a2cf92bdc448352afb55e6
nilkibite <96582027+nilkibite@users.noreply.github.com>
2022-09-28 23:21:55 -0600

    Create checkpoints.lua

---
+++ init/checkpoints.lua
@@ -0,0 +1,7 @@
+getgenv().config = {
+  set = Enum.KeyCode.E,
+  teleport = Enum.KeyCode.R,
+  unset = Enum.KeyCode.Q
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nilkibite/roblox_scripts/main/scripts/checkpoints.lua"))()
