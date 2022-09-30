a8e4c0a7dadcb6ee64d48e6cd6e735f10455cb9a
nilkibite <96582027+nilkibite@users.noreply.github.com>
2022-09-30 04:34:19 -0600

    Update checkpoints.lua

--- script/checkpoints.lua
+++ script/checkpoints.lua
@@ -1,7 +1,10 @@
 getgenv().config = {
-  set = Enum.KeyCode.E,
-  teleport = Enum.KeyCode.R,
-  unset = Enum.KeyCode.Q
+  keybinds = {
+    set = Enum.KeyCode.E,
+    teleport = Enum.KeyCode.R,
+    unset = Enum.KeyCode.Q
+  },
+  saveCameraAngle = true
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nilkibite/roblox_scripts/main/source/checkpoints.lua"))()
